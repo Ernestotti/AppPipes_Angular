f32930ba1c9341dbf745dbf881a41942c73057c0
Ernesto Fornas Castellanos <ernesto_13186@hotmail.com>
2022-10-25 13:42:54 +0200

    I18nPluralPipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -6,12 +6,21 @@ import { Component } from '@angular/core';
 })
 export class NoComunesComponent {
 
+  //i18nSelect
   nombre: string = 'Ernesto';
   genero: string = 'masculino';
-
   invitacionMapa = {
     'masculino': 'invitarlo',
     'femenino': 'invitarla'
   }
 
+  //i18nPlural
+  clientes: string[] = ['Marta', 'Ernesto', 'Charli'];
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    'other': 'tenemos # clientes esperando.',
+
+  }
 }
